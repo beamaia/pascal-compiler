f6e43345343b692d6c08951a448699dca585d5e8
Bea Maia <beamaia.work@gmail.com>
2022-07-02 21:58:20 -0300

    changes string type to char

--- in/ex15-procedures-and-functions.pas
+++ in/ex15-procedures-and-functions.pas
@@ -2,7 +2,7 @@ program exProcedure;
 var
    a, b, c,  min: integer;
 
-function min(num1, num2: integer; f1,f2 : string ): integer;
+function min(num1, num2: integer; f1,f2 : char ): integer;
 
 var
    (* local variable declaration *)
@@ -30,7 +30,7 @@ begin
       m:= z;
 end; { end of procedure findMin }  
 
-function max(num1, num2: integer; f1,f2 : string ): integer;
+function max(num1, num2: integer; f1,f2 : char ): integer;
 
 var
    (* local variable declaration *)
