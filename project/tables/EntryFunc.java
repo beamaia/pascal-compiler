cc6710d81091ff2f1b85c90a698c9f4bd3e23286
Bea Maia <beamaia.work@gmail.com>
2022-07-03 23:36:17 -0300

    adds return type
    Co-Authored-By: Victor Aguiar Marques <43154449+Victor0am@users.noreply.github.com>

--- project/tables/EntryFunc.java
+++ project/tables/EntryFunc.java
@@ -8,15 +8,18 @@ public class EntryFunc {
     public String name;
     public VarTable variableTable = new VarTable();
     public int line;
+    public Type type;
 
     /** 
     * Constructor for EntryFunc.
     * @param name The name of the entry.
     * @param line The starting line number of the function entry.
+    * @param type The return type of the function.
     */
-    public EntryFunc(String name, int line) {
+    public EntryFunc(String name, int line, Type type) {
         this.name = name;
         this.line = line;
+        this.type = type;
     }
 
     public boolean addVar(EntryInput entry) {
@@ -37,7 +40,7 @@ public class EntryFunc {
         StringBuilder sb = new StringBuilder();
 		Formatter f = new Formatter(sb);
         
-        f.format("EntryFunc %s -- name: %s, starting line: %d\n", name, name, line);
+        f.format("EntryFunc %s -- name: %s, starting line: %d, type: %s\n", name, name, line, type);
         f.format("-----------\n");
         f.format(variableTable.toString());
         f.close();
