bd8237fef5f9b07787f80bd9786cdc4b351c9387
Bea Maia <beamaia.work@gmail.com>
2022-07-12 23:10:58 -0300

    changes if example
    Co-Authored-By: Victor Aguiar Marques <43154449+Victor0am@users.noreply.github.com>

--- in/ex06-if-else.pas
+++ in/ex06-if-else.pas
@@ -4,27 +4,22 @@ var
     xOneBig,xTwoBig:boolean;
 
 begin
-    subTwo := xOne - xTwo;
 
     (* If com begins e ends *)
     if subTwo > 0 then
         begin
-            xOneBig:=1;
-            xTwoBig:=0;
+            xOneBig:=true;
+            xTwoBig:=false;
         end;
-    else if (subTwo < 0) then
-        begin   
-            xOneBig:=0;
-            xTwoBig:=1;
-        end;
+    
     else
         begin 
-            xOneBig:=0;
-            xTwoBig:=0;
+            xOneBig:=false;
+            xTwoBig:=false;
         end;
     
     (* If sem begin e end *)
-    if xOneBig then
+    {if xOneBig then
         writeln('Number 1 is bigger');
     else if xTwoBig then
         writeln('Number 2 is bigger');
@@ -44,19 +39,19 @@ begin
     if xOneBig=0 and xTwoBig=0 then
         writeln('They are equal');
         
-        (* If aninhado sem begin/end *)
-        if xOne div 2 = 1 then
-            writeln('Number 1 is odd');
+        (* If aninhado sem begin/end *)}
+        if (xOne * 2) = 1 then
+            xOne := 30;
         else 
-            writln('Number 1 is even');
+            xTwo := -30;
 
         (* If aninhado com begin/end *)
-        if xTwo div 2 = 1 then
+        {if xTwo div 2 = 1 then
             begin
                 writeln('Number 2 is odd');
             end;
         else
             begin 
                 writln('Number 2 is even');
-            end;
+            end;}
 end.
