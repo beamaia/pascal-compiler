68ae6ec44fac43ba1c3a92cb17cc8bcefa002d3c
Bea Maia <beamaia.work@gmail.com>
2022-07-12 22:22:09 -0300

    fixes procedure-func example
    Co-Authored-By: Victor Aguiar Marques <43154449+Victor0am@users.noreply.github.com>

--- in/ex15-procedures-and-functions.pas
+++ in/ex15-procedures-and-functions.pas
@@ -1,22 +1,7 @@
 program exProcedure;
 var
-   a, b, c,  min: integer;
+   a, b, c, min: integer;
    d: real;
-
-function min(num1, num2: integer; f1,f2 : char ): integer;
-
-var
-   (* local variable declaration *)
-   result: integer;
-
-begin
-   if (num1 < num2) then
-      result := num1;
-   
-   else
-      result := num2;
-   max := result;
-end;
 
 procedure findMin(x, y, z: integer; m: integer); 
 (* Finds the minimum of the 3 values *)
@@ -54,8 +39,7 @@ end;
 begin
    writeln(' Enter three numbers: ');
    readln( a, b, c);
-   findMin(a, b, c, min); (* Procedure call *)
-   a:= min + max(a, b, 'a', 'b'); (* Function call *)
+   a:=  max(a, b, 'a', 'b'); (* Function call *)
    c:= min + max(a, b, 'a', 'b'); (* Function call *)
-   writeln(' Minimum: ', min);
+   writeln(' Minimum: ', A);
 end.
