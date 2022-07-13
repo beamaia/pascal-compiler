523b4046b8ada126eeda9362feb5566fef603233
Bea Maia <beamaia.work@gmail.com>
2022-07-12 22:21:46 -0300

    adds diff types of while loop
    Co-Authored-By: Victor Aguiar Marques <43154449+Victor0am@users.noreply.github.com>

--- in/ex04-loop-while.pas
+++ in/ex04-loop-while.pas
@@ -4,10 +4,16 @@ var
 
 begin
    a := 10;
+   a := 20;
    while  a < 20  do
    
-   begin
-      writeln('value of a: ', a);
-      a := a + 1;
-   end;
+      begin
+         writeln('value of a: ', a);
+         a := a + 1;
+      end;
+
+   while  a < 20  do
+      a:=a+2;
+
+   a := 30;
 end.
